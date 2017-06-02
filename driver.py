b5dff624251d932dc89e1a8659a49b2d9121a961
Bence Gobolos <bence.gobolos@hightec-rt.com>
2017-06-02 16:32:07 +0200

    Extend typofinder scripts * trainer.py: ** add dry-run feature ** train dictionary from a directory's text files ** refactore is_dictionary_exists variable: deleted * driver.py: ** add ignore-words feature * src/linguist.py ** improve table-formatted output * htc-dictionary.json ** remove inlinable word

--- driver.py
+++ driver.py
@@ -62,6 +62,17 @@ def get_arguments():
                              'the known words (\'htc-dictionary.json\' is given by default).',
                         type=str, default=os.path.join(os.path.dirname(__file__), 'htc-dictionary.json'),
                         metavar='DICTIONARY_FILE')
+    parser.add_argument('-i', '--ignore',
+                        help='ignore a set of words when searching for typos. '
+                             'There words will not be added to the dictionary.',
+                        nargs='*', metavar='WORD')
+    parser.add_argument('-c', '--context',
+                        help='print to console the row where the typo was found.',
+                        action='store_true')
+    parser.add_argument('-t', '--table',
+                        help='print to console the result table containing the unknown words '
+                             'and the suggestions (if available).',
+                        action='store_true')
     parser.add_argument('input', help='A file or a directory you want to check.',
                         metavar='INPUT')
 
@@ -113,6 +124,11 @@ def main():
     linguist = Linguist()
     linguist.load_dictionary_from_json(args.dictionary)
 
+    if args.ignore:
+        add_word_list = [word.lower() for word in args.ignore]
+        linguist.train_dictionary(add_word_list)
+        _log.info("The following words will be ignored: %s." % ', '.join(add_word_list))
+
     if os.path.isdir(args.input):
         file_path_list = find_text_file_abs_paths(args.input, args.ext)
     else:
@@ -121,7 +137,10 @@ def main():
     for file_path in file_path_list:
         typofinder = Typofinder(linguist, file_path)
         typofinder.execute()
-        typofinder.print_affected_rows()
+        if args.context:
+            typofinder.print_affected_rows()
+        if args.table:
+            typofinder.print_result_map()
 
 if __name__ == "__main__":
     main()
